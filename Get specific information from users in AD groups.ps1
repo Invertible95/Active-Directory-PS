1754b5886c3a5fd0cd2cfeeb01e65530b7b7018a
Victor <91550933+Invertible95@users.noreply.github.com>
2023-03-10 08:30:34 +0100

    Update Get specific information from users in AD groups.ps1

--- Get specific information from users in AD groups.ps1
+++ Get specific information from users in AD groups.ps1
@@ -1,8 +1,12 @@
-﻿#Specific users from securitygroups
+# This script gets specific information for users member of a specific AD group
+# Attributes after Select-Object are examples and can be changed to your liking
+# The script also exports the information to a .csv file.
+# If you don't want the file exported you can put a # before the last pipe
 
 $Out = "Enter path for storing Awesometest.csv"
+$ADGroup = "Enter name of AD group"
 
-Get-ADGroupMember -Identity "Enter name of AD Group" -Recursive | 
+Get-ADGroupMember -Identity $ADGroup -Recursive | 
 
 Get-ADUser -Property * | Select-Object Name, Department, Office, Title, EmailAddress |
 
