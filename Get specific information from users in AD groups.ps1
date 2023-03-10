cefc0c94f9e74748ce912b5457549705802fc451
Victor <91550933+Invertible95@users.noreply.github.com>
2023-03-10 08:24:30 +0100

    Add files via upload

---
+++ Get specific information from users in AD groups.ps1
@@ -0,0 +1,9 @@
+﻿#Specific users from securitygroups
+
+$Out = "Enter path for storing Awesometest.csv"
+
+Get-ADGroupMember -Identity "Enter name of AD Group" -Recursive | 
+
+Get-ADUser -Property * | Select-Object Name, Department, Office, Title, EmailAddress |
+
+Export-Csv -Path $Out -Delimiter ";" -Encoding UTF8 
