f0ce4b9e0102b53d925dd51e7f743a422becca76
Victor <91550933+Invertible95@users.noreply.github.com>
2023-03-10 08:26:21 +0100

    Update Copy user groups to another.ps1

--- Copy user groups to another.ps1
+++ Copy user groups to another.ps1
@@ -1,3 +1,6 @@
+# This can be used to copy security groups from one user to another
+# Useful if you need to copy for example accessrights from an employee to a new 
+
 
 $CopyFrom = Read-host -Prompt "Enter user to copy from"
 $CopyTo = Read-host -Prompt "Enter user to copy to"
