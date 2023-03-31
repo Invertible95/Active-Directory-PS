9f7ee1a1e11d7afaed254871bf36deb7e3e81b65
Invertible95 <91550933+Invertible95@users.noreply.github.com>
2023-03-31 11:18:05 +0200

    Testing upload

--- Copy user groups to another.ps1
+++ Copy user groups to another.ps1
@@ -1,11 +1,13 @@
 # This can be used to copy security groups from one user to another
 # Useful if you need to copy for example accessrights from an employee to a new 
 
+function CopyADGroups {
+    $CopyFrom = Read-host -Prompt "Enter user to copy from"
+    $CopyTo = Read-host -Prompt "Enter user to copy to"
+    
+    Get-ADUser -Identity $CopyFrom -Properties memberof | Select-Object -ExpandProperty memberof |  Add-ADGroupMember -Members $CopyTo
+}
 
-$CopyFrom = Read-host -Prompt "Enter user to copy from"
-$CopyTo = Read-host -Prompt "Enter user to copy to"
-
-Get-ADUser -Identity $CopyFrom -Properties memberof | Select-Object -ExpandProperty memberof |  Add-ADGroupMember -Members $CopyTo
 
 
 ##Disclaimer##
